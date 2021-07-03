9ff415ed464905302052e8b722037577a7e94a2d
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2021-07-04 06:20:41 +0900

    ログファイルが存在しない場合は作成するようにした

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+cd $(dirname $0)
+
+if ! [ -f ./www/cgi-bin/log ]; then
+  printf '' > ./www/cgi-bin/log
+fi
+
 docker run --rm -t -v $(pwd)/www:/var/www/localhost/htdocs -v $(pwd)/conf/lighttpd.conf:/etc/lighttpd/lighttpd.conf -v $(pwd)/conf/mod_cgi.conf:/etc/lighttpd/mod_cgi.conf -p 8080:80 sebp/lighttpd
